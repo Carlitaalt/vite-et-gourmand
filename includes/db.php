6b3cf59457e81b855f03d015a3de9eae662e9914
Carla Ferrero <carlaferrero003@gmail.com>
2026-05-04 10:39:57 +0200

    modification db.name

--- includes/db.php
+++ includes/db.php
@@ -10,19 +10,13 @@ $dbname = 'if0_41824942_vite_gourmand';
 $user = 'if0_41824942';
 $password = 'thoy8NILVbiQ';
 
-$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_PERSISTENT => false, //Évite les fuites de mémoires
-];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
 } catch (PDOException $e) {
-    error_log('Erreur BDD : ' . $e->getMessage());
-    die('Service indisponible. Veuillez réessayer plus tard.');
+    die('Erreur SQL : ' . $e->getMessage());
 }
